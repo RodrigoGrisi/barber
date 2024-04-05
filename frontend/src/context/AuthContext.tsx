08c8ccf72bc6a6c49b22951812693ac99f538b59
rodrigogrisi <Rodricosta1995@gmail.com>
2024-04-04 22:52:30 -0300

    Feat: Starting the Authcontext

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,37 @@
+import { Children, createContext, ReactNode, useState } from 'react'
+
+interface AuthContextData {
+  user: UserProps
+  isAuthenticated: boolean
+}
+
+interface UserProps {
+  id: string
+  name: string
+  email: string
+  endereco: string | null
+  subscriptions?: subscriptionProps | null
+}
+
+interface subscriptionProps {
+  id: string
+  status: string
+}
+
+type AuthProviderProps = {
+  children: ReactNode
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<UserProps>()
+  const isAuthenticated = !!user
+
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated }} >
+      {children}
+    </AuthContext.Provider>
+  )
+}
